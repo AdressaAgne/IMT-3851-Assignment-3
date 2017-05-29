7e679570ca92ff2023cc06b3ae02d024983928c2
Audun Olsen <audunmeekolsen@gmail.com>
2017-05-29 04:17:00 +0200

    Add first draft of navigation drawer in addition to other misc sass changes

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('sup')
+// Activate navigation drawer when hamburger is clicked
+$('.hamburger').on('click', function() {
+	$('nav.overlay').addClass('active');
+});
+
+// Deactivate navigation drawer when overlay (not drawer) is clicked
+$('nav.overlay').on('click', function() {
+	$('nav.overlay').removeClass('active')
+}).children().click(function(e) {
+	return false;
+});	
